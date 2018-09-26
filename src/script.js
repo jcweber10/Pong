5417bbd2934deb24a65b31e99af6656cd45ee0bb
JC Weber <jcweber10@gmail.com>
2018-09-26 17:41:35 -0400

    Revert "Resize to smaller"
    This reverts commit 44718ac6c4fcfe299d452236e52e5ed688af94d2.

--- src/script.js
+++ src/script.js
@@ -1,13 +1,9 @@
 var canvas = document.getElementById("myCanvas");
-canvas.width = window.innerWidth / 2;
-canvas.height = window.innerHeight / 2;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;;
 var ctx = canvas.getContext("2d");
-let redrawCount = 0;
 // boolean to see if game has started yet
 let isLive = false;
-
-// Change in x or y
-let change = 2;
 
 // Keypressed booleans
 let upPressed = false;
@@ -15,14 +11,18 @@ let downPressed = false;
 let wPressed = false;
 let sPressed = false;
 
-// Paddle properties
-let paddleHeight = 76;
-let paddleWidth = 10;
+// Paddle 1 properties
+let paddle1Height = 76;
+let paddle1Width = 10;
+
+// Paddle 2 properties
+let paddle2Height = paddle1Height;
+let paddle2Width = paddle1Width;
 
 
 let p1Score = 0;
 let p2Score = 0;
-let paddle1Y = canvas.height / 2 - paddleHeight / 2;
+let paddle1Y = canvas.height / 2 - paddle1Height / 2;
 let paddle2Y = paddle1Y;
 // Ball properties
 let ballRadius = 10;
@@ -34,23 +34,23 @@ initBoard();
 
 // Draws the objects in their starting states
 function initBoard() {
-    paddle1Y = canvas.height / 2 - paddleHeight / 2;
+    paddle1Y = canvas.height / 2 - paddle1Height / 2;
     paddle2Y = paddle1Y;
     ballX = canvas.width / 2;
     ballY = canvas.height / 2;
     let ranNumb = Math.floor(Math.random() * 4);
     switch (ranNumb) {
-        case 0: dy = -change;
-            dx = -change;
+        case 0: dy = -2;
+            dx = -2;
             break;
-        case 1: dy = -change;
-            dx = change;
+        case 1: dy = -2;
+            dx = 2;
             break;
-        case 2: dy = change;
-            dx = -change;
+        case 2: dy = 2;
+            dx = -2;
             break;
-        case 3: dy = change;
-            dx = change;
+        case 3: dy = 2;
+            dx = 2;
             break;
     }
     draw();
@@ -89,11 +89,11 @@ function verticalCollision() {
 }
 
 function hitsLeftPaddle() {
-    return ballX <= 0 + paddleWidth && (ballY >= paddle1Y - 4 && ballY <= paddle1Y + paddleHeight + 4);
+    return ballX <= 0 + paddle1Width && (ballY >= paddle1Y - 4 && ballY <= paddle1Y + paddle1Height + 4);
 }
 
 function hitsRightPaddle() {
-    return ballX >= canvas.width - paddle2Width && (ballY >= paddle2Y - 4 && ballY <= paddle2Y + paddleHeight + 4);
+    return ballX >= canvas.width - paddle2Width && (ballY >= paddle2Y - 4 && ballY <= paddle2Y + paddle2Height + 4);
 }
 
 // Collision with wall or paddle
@@ -108,7 +108,7 @@ function leftCollision() {
         alert("Congratulations player 1! YOU WIN!");
         p2Score = 0;
         // document.location.reload();
-        window.location = "index.html";
+        window.location = "welcome.html";
     }
 }
 
@@ -124,7 +124,7 @@ function rightCollision() {
         alert("Congratulations player 1! YOU WIN!");
         p1Score = 0;
         // document.location.reload();
-        window.location = "index.html";
+        window.location = "welcome.html";
     }
 }
 
@@ -145,14 +145,14 @@ function accelerateBall(rate) {
 function drawPaddles() {
     //  Draw left paddle
     ctx.beginPath();
-    ctx.rect(0, paddle1Y, paddleWidth, paddleHeight);
+    ctx.rect(0, paddle1Y, paddle1Width, paddle1Height);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.closePath();
 
     // Draw right paddle
     ctx.beginPath();
-    ctx.rect(canvas.width - paddle2Width, paddle2Y, paddle2Width, paddleHeight);
+    ctx.rect(canvas.width - paddle2Width, paddle2Y, paddle2Width, paddle2Height);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.closePath();
@@ -171,13 +171,13 @@ function updatePositions() {
     if (upPressed && paddle2Y - 1 > 0) {
         paddle2Y -= 7;
     }
-    if (downPressed && paddle2Y + paddleHeight + 1 < canvas.height) {
+    if (downPressed && paddle2Y + paddle2Height + 1 < canvas.height) {
         paddle2Y += 7;
     }
     if (wPressed && paddle1Y - 1 > 0) {
         paddle1Y -= 7;
     }
-    if (sPressed && paddle1Y + paddleHeight + 1 < canvas.height) {
+    if (sPressed && paddle1Y + paddle1Height + 1 < canvas.height) {
         paddle1Y += 7;
     }
 }
@@ -196,7 +196,7 @@ function draw() {
 
     // Adjust y values based on keyPresses
     updatePositions();
-    // paddle1Y = ballY - paddleHeight / 2;
+    // paddle1Y = ballY - paddle1Height / 2;
     // Call any collisions and draw objects
     verticalCollision();
     leftCollision();
@@ -206,14 +206,10 @@ function draw() {
         drawBall();
     }
 
-    if (redrawCount % 10 === 0) {
-        accelerateBall(.000000);
-    }
+    accelerateBall(.0003);
     // Moves the ball
     ballX += dx;
     ballY += dy;
-
-    redrawCount++;
 
 }
 document.addEventListener("keydown", keyDownHandler, false);
